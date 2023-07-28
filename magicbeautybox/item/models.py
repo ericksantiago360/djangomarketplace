30969dd2f66c8d3e5f0c1a4143e5e13383948d00
Erick Santiago  Torres <ericksan360@gmail.com>
2023-07-28 09:20:33 -0600

    ok

--- magicbeautybox/item/models.py
+++ magicbeautybox/item/models.py
@@ -19,7 +19,7 @@ class Item (models.Model):
     image = models.ImageField(upload_to='item_images',blank= True ,null= True )
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User ,related_name='items', on_delete=models.CASCADE )
-    created_by= models.DateTimeField(auto_now_add= True)
+    created_at = models.DateTimeField(auto_now_add= True)
         
     def __str__(self):  
         return self.name 
